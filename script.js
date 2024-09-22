b1cae2f76878caccf7131397b822c6df4bb522c7
Ateş Parıltı <atesparilti@gmail.com>
2024-09-22 20:42:22 +0300

    logical operators and switch statement

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ if(js === "amazing") alert("Javascript is fun!!");
 
         
 console.log(40+8+23-10);
-*/
-/*
+
+
 const age= "18";
 if(age === 18) console.log("You just become an adult (strict)");
 if(age == 18) console.log("You just become an adult (loose)");
@@ -20,7 +20,7 @@ if (favorite == 23) {
 } else {
     console.log("Your favorite number is not 23 or 7")
 }
-*/
+
 
 const hasDriversLisence = true;
 const hasGoodVision = true;
@@ -36,3 +36,39 @@ if(shouldDrive){
 }else {
     console.log("Someone else should drive!")
 }
+
+const day = "monday";
+switch(day) {
+    case "monday":
+        console.log("Go to school");
+        console.log("Go do some coding");
+        console.log("Go to the gym");
+        break;
+    case "tuesday":
+        console.log("Go to school");
+        console.log("Go do some coding");
+        break;
+    case "wednesday":
+        console.log("Go to school");
+        console.log("Go do some coding");
+        console.log("Go to the gym");
+        break;
+    case "thirsday":
+        console.log("Go to school");
+        console.log("Go do some coding");
+        break;
+    case "friday":
+        console.log("Go to school");
+        console.log("Go to the gym");
+        break; 
+    case "saturday":
+        console.log("Practive AP");
+        console.log("Go do some coding");
+        break; 
+    case "sunday":
+        console.log("Practice AP");
+        console.log("Go to the gym");
+        break;
+         
+}
+*/
